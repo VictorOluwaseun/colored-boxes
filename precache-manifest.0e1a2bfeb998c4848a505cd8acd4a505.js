b1eb33f18eade1694b198a22b631d6007bb53bbc
Victor <adevic4christ@gmail.com>
2019-12-01 20:04:45 +0100

    Updates

---
+++ precache-manifest.0e1a2bfeb998c4848a505cd8acd4a505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247ba2f9ae3f8b717aa173ca5b34eda6",
+    "url": "//victorpro4C.github.io/colored-boxes.git/index.html"
+  },
+  {
+    "revision": "cf0d67f4aa9b8ea0c974",
+    "url": "//victorpro4C.github.io/colored-boxes.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "34ed2eb2e57cbc60fc38",
+    "url": "//victorpro4C.github.io/colored-boxes.git/static/js/2.0baed8b2.chunk.js"
+  },
+  {
+    "revision": "cf0d67f4aa9b8ea0c974",
+    "url": "//victorpro4C.github.io/colored-boxes.git/static/js/main.8879103d.chunk.js"
+  },
+  {
+    "revision": "d646ae52ca2635cf5498",
+    "url": "//victorpro4C.github.io/colored-boxes.git/static/js/runtime-main.cc444c06.js"
+  }
+]);
